ac6bed144af5a3f20dbd8228bf61c25b8be24c9b
wajuu <86391160+wajuu@users.noreply.github.com>
2021-07-26 18:12:32 +0900

    Add files via upload

--- js/motion.js
+++ js/motion.js
@@ -16,3 +16,20 @@ $(document).ready(function(){
     });
 
 });
+
+
+
+
+$(document).ready(function(){
+
+    $('#login').click(function(){
+               
+    var popupX = (document.body.offsetWidth / 2) - (350 / 2);
+    //만들 팝업창 좌우 크기의 1/2 만큼 보정값으로 빼주었음
+
+    var popupY= (window.screen.height / 2) - (350 / 2);
+    //만들 팝업창 상하 크기의 1/2 만큼 보정값으로 빼주었음
+
+    window.open('login.html', '로그인', 'status=no, height=350, width=350,scrollbars=no left='+ popupX + ', top='+ popupY);
+});
+});
